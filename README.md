638d59775d412ef212f00b47502384c992144062
apella1 <johncollons5@gmail.com>
2023-04-05 17:19:10 +0300

    Setup postgres

---
+++ README.md
@@ -0,0 +1,20 @@
+# Django E-commerce Backend
+
+This repository contains the backend codebase for a modern e-commerce web application built with Django. 
+The backend handles various microservices including product catalog, cart management, order management,
+payment gateway integration, user authentication, search services, review services, analytics services,
+inventory services, and shipping services.
+
+## Getting Started
+
+### Installation
+
+- Clone the repository: `git clone https://github.com/apella1/django-ecommerce.git`
+- Working in a virtual environment, install dependencies: `pip install -r requirements.txt`
+
+### Contributing
+
+Contributions are welcome! If you encounter any bugs, please open an issue or submit a pull request.
+
+### License
+This project is licensed under the MIT License
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +70,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+# Allowed urls
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173"
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-ecommerce',
+        'USER': 'apella',
+        'PASSWORD': 'm124@neezy',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -110,7 +118,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
